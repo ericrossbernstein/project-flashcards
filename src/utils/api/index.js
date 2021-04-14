789b0dc8505433b0364de670fbdbf6196d5c1b55
Eric Ross Bernstein <ericrossbernstein@gmail.com>
2021-04-14 12:29:45 -0700

    Updated index.html

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -2,9 +2,7 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL =
-  process.env.API_BASE_URL ||
-  "https://ericrossbernstein.github.io/project-flashcards";
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
