245025c97358728727e0677bb35f13ab57e7ff34
Eric Ross Bernstein <ericrossbernstein@gmail.com>
2021-04-15 14:26:54 -0700

    Updated dotenv import for ES6 syntax

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
