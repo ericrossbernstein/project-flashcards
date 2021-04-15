d632bd2b974fbaf49b002bf37dd9fcb8aae2011d
Eric Ross Bernstein <ericrossbernstein@gmail.com>
2021-04-15 11:36:50 -0700

    Added # to base URL environment variable

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Layout from "./Layout";
 import "./App.css";
-require("dotenv").config();
 
 /**
  * App is a wrapper for <Layout>, you should not need to change this file.
